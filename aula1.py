843d8f7d50bc1228cc5cfb349587edf87e3a19d3
Guilherme <guivjjig@gmail.com>
2025-02-19 11:23:48 -0300

    melhoria

--- aula1.py
+++ aula1.py
@@ -1,5 +1,7 @@
 import random
 sorteio = random.randint (1,10)
+print ("####### Jogo da Adivinhacao ######")
+print ("Adivinhe o numero que estou pensando")
 chute = input (" Digite seu chute ")
 print (sorteio)
 print (chute)
