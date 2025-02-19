24d11f33b6903244fc4b5170daf13b86007a1048
Guilherme <guivjjig@gmail.com>
2025-02-19 12:10:45 -0300

    oooooooooooooooooo

--- aula1.py
+++ aula1.py
@@ -3,13 +3,20 @@ sorteio = random.randint (1,10)
 print ("####### Jogo da Adivinhacao ######")
 print ("Adivinhe o numero que estou pensando")
 chute = int( input (" Digite seu chute "))
+limite_tentativas = 3
+tentativas = 1
+while (limite_tentativas >= tentativas)
+chute = int( input (" Digite seu chute "))
+
+print ("tentativas:", tentativas)
+#tentativas = tentativas + 1
 #print (sorteio)
 #print(chute)
 print (sorteio)
 print (chute)
 if (chute == sorteio):
     print("Parabens , voce  acertou numero era",sorteio)
-if (chute! = sorteio):
-    
-    print ("voce errou",sorteio)
+
+
+
         
