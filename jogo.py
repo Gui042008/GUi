348f39e49f6bcbb510733fad802439c5c5ebcab8
Guilherme <guivjjig@gmail.com>
2025-02-26 12:17:23 -0300

    ooii

---
+++ jogo.py
@@ -0,0 +1,28 @@
+import random 
+numero_max = 10
+sorteio = random.randint(1, numero_max)
+
+#chute = input("digite o seu chute:")
+#print(sorteio)
+print ("### JOGO DA ADIVINHAÇÃO ###")
+print("Adivinhe o número que estou pensando,de 1 a 10")
+chute=int (input("chute"))
+limite_tentativas= 3
+tentativas = 1
+while (limite_tentativas >= tentativas ):
+    print("tentativa:",tentativas)
+    chute= int(input("digite o seu chute"))
+    if (chute==sorteio):
+        print("Parabens,voce acertou")
+    #tentativas = tentativas +1
+    #print (sorteio)
+    #print(chute)
+    if (chute==sorteio):
+       print("Parabens,voce acertou qual numero era",)
+       break
+    elif (chute > sorteio):
+        print("chute um numero menor")
+    elif(chute>sorteio):
+        print("chute um numero maior")
+        tentativas=tentativas + 1
+ 
